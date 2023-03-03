1e6bfa7d57c892fc2a5ee01e89133ba733290bc7
Caleb Farrell <caleb.farrell987@gmail.com>
2023-03-02 21:33:29 -0400

    Added: a note in the init file of the resources dir

---
+++ resources/__init__.py
@@ -0,0 +1,2 @@
+#Including an init file here as I will be adding logic to pull in the csv from a URL and cache it locally for use in the script. This will allow for the script to be run without the need to download the csv file manually. 
+#It'll also check the cache checksum value vs. the CDN version and update if needed.
