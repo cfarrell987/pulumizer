f1c0cda55adc41296ff20f7f961a73a0ef1973e1
Caleb Farrell <caleb.farrell987@gmail.com>
2023-03-05 09:39:04 -0400

    Added pre-commit

--- APC/main.py
+++ APC/main.py
@@ -1,7 +1,8 @@
 from aws import aws_dump  # pylint: disable=import-error
-from tools import json,logger  # pylint: disable=import-error
+from tools import json, logger  # pylint: disable=import-error
+
 if __name__ == "__main__":
     # TODO: Add support for multiple regions
     dump = aws_dump.get_resources("ca-central-1", "default")
-    output = json.json_write(dump, 'resources.json')
+    output = json.json_write(dump, "resources.json")
     logger = logger.get_logger("apc.log")
--- APC/tools/json.py
+++ APC/tools/json.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 import json
 
+
 def json_write(resources, filename):
-    with open(filename, 'w', encoding='UTF8') as file:
+    with open(filename, "w", encoding="UTF8") as file:
         json.dump(resources, file, indent=4)
 
-    return 'File written to: ' + str(Path(filename).absolute())
+    return "File written to: " + str(Path(filename).absolute())
