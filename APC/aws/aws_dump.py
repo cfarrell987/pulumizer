4631b5ac31c2110e0e0046682e4b9c27b011c69a
Caleb Farrell <caleb.farrell987@gmail.com>
2023-03-06 21:01:51 -0400

    Added: Comments and docstrings Added: boto3_init initializes the boto3 client prior to running the get_resources func Changed: Some parsing to match the pulumi bulk json format Changed: more disables in pylint.yml for unwanted checks

--- APC/aws/aws_dump.py
+++ APC/aws/aws_dump.py
@@ -5,8 +5,12 @@ from tools import logger  # pylint: disable=import-error
 
 logger = logger.get_logger("apc.log")
 
+"""
+Using the supported_services.csv file get all services
+that are supported by ResourceGroupsTaggingAPI
+"""
 
-# Using the ServiceQuotas API get all services within a specific region
+
 def get_services():
     # TODO: Move file to a CDN and pull on first run, verify checksum and update if needed
     with open("./resources/supported_services.csv", "r", encoding="UTF8") as file:
@@ -16,29 +20,39 @@ def get_services():
     return supported_services
 
 
-# Using ResourceGroupsTaggingAPI get all resources within a specific region
-def get_resources(region, profile):
-    # TODO: Add support for multiple profiles
+# TODO: Add support for multiple profiles
+def boto3_init(region, profile):
     boto3.setup_default_session(profile_name=profile)
     services = get_services()
-    print(services)
     client = boto3.client("resourcegroupstaggingapi", region_name=region)
-
     paginator = client.get_paginator("get_resources")
+
+    return services, paginator
+
+
+# Using ResourceGroupsTaggingAPI get all resources within a specific region
+def get_resources(services, paginator):
     resources = {"resources": []}
 
     for service in services:
-        paginator = client.get_paginator("get_resources")
         for response in paginator.paginate(ResourceTypeFilters=[service]):
+            """
+            Check if the pagination token is empty,
+            if it is then break the loop as this service is empty
+            """
             if response["PaginationToken"] == "":
                 logger.error("Pagination token is empty for: " + service)
                 break
 
             for resource in response["ResourceTagMappingList"]:
-                main_service = resource["ResourceARN"].split(":")[2]
                 arn = resource["ResourceARN"]
-                resource_service = resource["ResourceARN"].split(":")[5].split("/")[0]
-                resource_id = resource["ResourceARN"].split(":")[-1]
+                arn_block = arn.split(":")
+                main_service = arn_block[2]
+                service = arn_block[5].split("/")[0]
+                resource_type = (
+                    f"aws:{main_service.lower()}/{service}:{service.capitalize()}"
+                )
+                resource_id = resource["ResourceARN"].split(":")[-1].split("/")[1]
                 tags = {tag["Key"]: tag["Value"] for tag in resource["Tags"]}
                 name = tags.get("Name", resource_id)
 
@@ -53,7 +67,7 @@ def get_resources(region, profile):
                     resources[main_service] = []
                 resources.setdefault("resources", []).append(
                     {
-                        "Type": resource_service,
+                        "Type": resource_type,
                         "name": name,
                         "id": resource_id,
                         "ARN": arn,
--- APC/main.py
+++ APC/main.py
@@ -3,6 +3,7 @@ from tools import json, logger  # pylint: disable=import-error
 
 if __name__ == "__main__":
     # TODO: Add support for multiple regions
-    dump = aws_dump.get_resources("ca-central-1", "default")
+    services, paginator = aws_dump.boto3_init("ca-central-1", "passiv")
+    dump = aws_dump.get_resources(services, paginator)
     output = json.json_write(dump, "resources.json")
     logger = logger.get_logger("apc.log")
